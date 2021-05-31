27a76161ee9c3bc4dce52433482a8f1356506a2c
Kate-13 <kate_boyko@mail.ru>
2021-05-31 18:00:04 +0300

    feat: implement convertToHappy

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,39 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        val sadArrayList = ArrayList<Int>()
+        sadArray.forEachIndexed { i, el ->
+            sadArrayList.add(el)
+        }
+
+        var results = ArrayList<Int>()
+
+        while (true) {
+
+            var countChanges = 0
+
+            val happyarr = ArrayList<Int>()
+            sadArrayList.forEachIndexed { i, el ->
+                happyarr.add(el)
+            }
+
+            happyarr.forEachIndexed { i, el ->
+                if (i<(happyarr.count()-2)) {
+                    val sum = happyarr[i] + happyarr[i + 2]
+                    if (happyarr[i + 1] > sum) {
+                        sadArrayList.remove(happyarr[i + 1])
+                        countChanges++
+                    }
+                }
+            }
+
+            if(countChanges == 0) {
+                results = happyarr
+                break
+            }
+        }
+
+        return results.toIntArray()
     }
 }
